5fb48ed8e61dc8e73142d5c825972d26b07527cd
JuanCarlosJC-20 <sampfz23@gmail.com>
2026-09-23 10:58:07 -0500

    add fix/function-reset-password

--- src/main/java/com/somnguard/security/adapter/in/web/dto/ResetPasswordRequest.java
+++ src/main/java/com/somnguard/security/adapter/in/web/dto/ResetPasswordRequest.java
@@ -2,9 +2,10 @@ package com.somnguard.security.adapter.in.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record ResetPasswordRequest(
-        @NotBlank @JsonAlias({"token", "token"}) String token,
+        @NotBlank @Pattern(regexp = "^\\d{6}$", message = "code must be 6 digits") @JsonAlias({"token", "code"}) String token,
         @NotBlank @Size(min = 8, max = 72) @JsonAlias({"new_password", "newPassword"}) String newPassword
 ) {}
